b3d7b20838d59fa4c67543f550fb8e2a2afec328
caiqingsong <cqs@caiqingsongdeMacBook-Air.local>
2024-10-07 15:34:06 +0800

    10.7

--- agents/study.py
+++ agents/study.py
@@ -13,31 +13,85 @@ enc = tiktoken.get_encoding("cl100k_base")
 class Study:
 
     def __init__(self, abstract_url,drug_name,compressor=None):
-        self.abstract_url = abstract_url
-        self.drug_name = drug_name
         self.compressor = compressor
         self.data = {
+            "base_info":{
+                "abstract_url": abstract_url,
+                "drug_name": drug_name
+            },
+            "paper_info":{
             "abstract": "",
+            "conten":"",
             "includes_pediatrics": False,
             "proves_effective": False,
-            "patient_age": "",
-            "disease_icd10": "",
+            "age": "",
+            "disease": "",
+            "icd10":"",
             "drug_route": "",
-            "question_list": {}
+            "study_type": ""    
+        },
+        "state":{
+            "need_to_fetch_conent": False,
+            "include_abstract": False,
+            "include_content": False,
+            "process_pediatrics_inabstract": False,
+            "process_effectiveness": False,
+            "process_pediatrics_incontent": False,
+            "process_population_effectiveness": False,
+            "remain_problem": False
+        },
+        "answer":{
+            "process_pediatrics_inabstract":"",
+            "process_effectiveness":"",
+            "process_pediatrics_incontent":"",
+            "process_population_effectiveness":"",
+            "age":"",
+            "target_disease":"",
+            "icd10_of_target_disease":"",
+            "drug_intervention":"",
+            "study_type":""
         }
-        self.data["question_list"]["process_pediatrics_inabstract"] = False
-        self.data["question_list"]["process_effectiveness"] = False
-        self.data["question_list"]["process_pediatrics_incontent"] = False
-        self.data["question_list"]["process_population_effectiveness"] = False
-        self.data["question_list"]["remain_problem"] = False
+        }
+        # self.paper_info = {
+        #     "abstract": "",
+        #     "conten":"",
+        #     "includes_pediatrics": False,
+        #     "proves_effective": False,
+        #     "age": "",
+        #     "disease": "",
+        #     "icd10":"",
+        #     "drug_route": "",
+        #     "study_type": ""    
+        # }
+        # self.state ={
+        #     "need_to_fetch_conent": False,
+        #     "include_abstract": False,
+        #     "include_content": False,
+        #     "process_pediatrics_inabstract": False,
+        #     "process_effectiveness": False,
+        #     "process_pediatrics_incontent": False,
+        #     "process_population_effectiveness": False,
+        #     "remain_problem": False
+        # }
+        # self.answer ={
+        #     "process_pediatrics_inabstract":"",
+        #     "process_effectiveness":"",
+        #     "process_pediatrics_incontent":"",
+        #     "process_population_effectiveness":"",
+        #     "age":"",
+        #     "target_disease":"",
+        #     "icd10_of_target_disease":"",
+        #     "drug_intervention":"",
+        #     "study_type":""
+        # }
 
 
 
     def fetch_abstract(self,abstract=None):
         if abstract!=None:
-            self.data["abstract"] = abstract
+            self.data["paper_info"]["abstract"] = abstract
             return True
-        url = 'https://pubmed.ncbi.nlm.nih.gov/' + self.abstract_url + "/" 
+        url = 'https://pubmed.ncbi.nlm.nih.gov/' + self.data["base_info"]["abstract_url"] + "/" 
         try:
             response = requests.get(url)
             if response.status_code == 200:
@@ -47,54 +101,77 @@ class Study:
                 if abstract_div is not None:
                     html_abstract = abstract_div.get_text()
                 else:
-                    logging.error(f"drug name:{self.drug_name}'s paper{self.abstract_url} not have abstract")
+                    logging.error(f"drug name:{self.data["base_info"]["drug_name"]}'s paper{self.data["base_info"]["abstract_url"]} not have abstract")
                     return False
                 markdown = html2text.html2text(html_abstract)  
-                self.data["abstract"] = markdown
+                self.data["paper_info"]["abstract"] = markdown
+                self.data["state"]["include_abstract"] = True
                 logging.info(f"Abstract: {markdown} have been fetched.")
                 return True
         except requests.RequestException as e:
             logging.error(f"Error fetching abstract: {e}")
             return False
+    
+    def fetch_content(self,content=None):
+        if content!=None:
+            self.data["content"] = content
+            self.state["include_content"] = True
+            return True
+        """"""
 
 
 
     def process_pediatrics_inabstract(self):
+        if self.data["state"]["process_pediatrics_inabstract"] == True:
+            json_object = self.answer["process_pediatrics_inabstract"]
+            return json_object
         json_object = {"reason": "","short_answer": "[final model answer (e.g. included,excluded,age_not_mentioned]"}
         prompt = "##prompt:Use the criteria below to inform your decision. If any exclusion criteria are met or not all inclusion criteria are met, exclude the article, lf all inclusion criteria are met, include the article. Please type \"included\"or \"excluded\" to indicate your decision. ##Abstract:{abstract}\n\n##Inclusion criteria:\nDrug Efficacy Study in human\nAt least one pediatric(<18 years) in sample patients.\n\n##Attention:If no age information is mentioned in the abstract,return \"age_not_mentioned\"\n##model generated chain of thought explanation.\nTherefore, the json format answer is \n{json}"
-        prompt = prompt.format(abstract=self.data["abstract"],json=json_object)
+        prompt = prompt.format(abstract=self.data["paper_info"]["abstract"],json=json_object)
         prompt_dict = {}
         prompt_dict["prompt"] = prompt
         result = gpt_request_from_relative_chunk(prompt_dict)
         try:
             json_object = json.loads(result)
+            self.data["answer"]["process_pediatrics_inabstract"] = json_object
+            return json_object
         except json.JSONDecodeError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
 
-        return json_object
+        
 
     def process_effectiveness(self):
+        if self.data["state"]["process_effectiveness"] == True:
+            json_object = self.data["answer"]["process_effectiveness"]
+            return json_object
         json_object = {"reason": "","short_answer": "[final model answer (e.g. Yes,No,NA,NM)]"}
         prompt = "##question:Only using the information provided in \"Text for Analysis\", assess whether the target drug {target_drug}, shows significant efficacy or effectiveness (including improvement in survival) in pediatric populations.\nLet's think step by step. Follow these guidelines:\n**Control group: Identify whether this study divided patients into two or more groups receiving different intervention regimens (also known as treatment regimens), and whether this study included a control group or more.\nIs target drug involved: Examine the intervention regimens across all groups in the study, identify which regimens in this study involve the target drug {target_drug}. This includes groups receiving the drug as monotherapy, as part of combination therapy with other medications, or as an active ingredient in a regimen that also includes a placebo.\nEffective: If the text shows that the regimen including {target_drug} has significant efficacy or effectiveness (including improvement in survival or other clinical outcome measures) compared to a control group that does not involving {target_drug} in pediatric patients, state that the regimens including this drug is more effective than other regimens that do not include this drug in this population.\nNA (Not Applicable): If the study is a single-arm study or does not include a control group, or if all intervention regimens include the target drug {target_drug}, respond with \"NA\".\nNM (Not Mentioned): If the text does not mention {target_drug}, or if the drug is not the subject of the study, or if the study is not conducted on pediatric populations, respond with \"NM\".\nPlease return \"Yes\", \"No\", \"NA (Not Applicable)\", or \"NM (Not Mentioned)\", and provide your assessment based on the provided text and explain your reasoning.\n##Text for Analysis:\n{abstract}\n\n##Your Assessment:\nmodel generated chain of thought explanation\nTherefore, the json format answer is \n{json}"
-        prompt = prompt.format(abstract=self.data["abstract"],json=json_object,target_drug=self.drug_name)
+        prompt = prompt.format(abstract=self.data["abstract"],json=json_object,target_drug=self.data["base_info"]["drug_name"])
         prompt_dict = {}
         prompt_dict["prompt"] = prompt
         result = gpt_request_from_relative_chunk(prompt_dict)
         try:
             json_object = json.loads(result)
+            self.data["answer"]["process_effectiveness"] = json_object
+            return json_object
         except json.JSONDecodeError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
 
-        return json_object
+        
 
  
 
     def process_pediatrics_incontent(self):
+        if self.data["state"]["process_pediatrics_incontent"] == True:
+            json_object = self.answer["process_pediatrics_incontent"]
+            return json_object
         json_object = {"reason":"","short_answer":""}
         question = "Is the pediatric (<18 years) population included in this study?"
-        relative_chunk = find_relative_chunk( os.path.join("/LLM-Medical-Agent/data/json_100",self.drug_name,self.abstract_url+".json"),question)
+        if self.data["state"]["include_content"] == False:
+            return None
+        relative_chunk = find_relative_chunk( os.path.join("/LLM-Medical-Agent/data/json_100",self.data["base_info"]["drug_name"],self.data["base_info"]["abstract_url"]+".json"),question)
         content = []
         all_token = 0
         for key, value in relative_chunk.items():
@@ -122,33 +199,40 @@ class Study:
         result = gpt_request_from_relative_chunk(prompt_dict)
         try:
             json_object = json.loads(result)
+            self.data["answer"]["process_pediatrics_incontent"] = json_object
+            return json_object
         except json.JSONDecodeError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
         # json_obj = {'reason': 'The study specifically focused on preterm infants, categorizing them by gestational age and birth weight. Since pediatric population generally refers to children from birth to adolescence, and in this context, all subjects are preterm infants (which are newborns), it is correct to interpret that this pediatric subset was included in the study.', 'short_answer': 'yes'}
-        return json_object
+        
 
     def process_population_effectiveness(self):
+        if self.data["state"]["process_population_effectiveness"] == True:
+            json_object = self.data["answer"]["process_population_effectiveness"]
+            return json_object
         json_object = {"reason":"","short_answer":""}
         prompt = "##question:Only using the information provided in \"Text for Analysis\", assess whether the target drug {target_drug}, shows significant efficacy or effectiveness (including improvement in survival) in populations.\nLet's think step by step. Follow these guidelines:\n**Control group: Identify whether this study divided patients into two or more groups receiving different intervention regimens (also known as treatment regimens), and whether this study included a control group or more.\nIs target drug involved: Examine the intervention regimens across all groups in the study, identify which regimens in this study involve the target drug {target_drug}. This includes groups receiving the drug as monotherapy, as part of combination therapy with other medications, or as an active ingredient in a regimen that also includes a placebo.\nEffective: If the text shows that the regimen including {target_drug} has significant efficacy or effectiveness (including improvement in survival or other clinical outcome measures) compared to a control group that does not involving {target_drug} in  patients, state that the regimens including this drug is more effective than other regimens that do not include this drug in this population.\nNA (Not Applicable): If the study is a single-arm study or does not include a control group, or if all intervention regimens include the target drug {target_drug}, respond with \"NA\".\nNM (Not Mentioned): If the text does not mention {target_drug}, or if the drug is not the subject of the study, or if the study is not conducted on populations, respond with \"NM\".\nPlease return \"Yes\", \"No\", \"NA (Not Applicable)\", or \"NM (Not Mentioned)\", and provide your assessment based on the provided text and explain your reasoning.\n##Text for Analysis:\n{abstract}\n\n##Your Assessment:\nmodel generated chain of thought explanation\nTherefore, the json format answer is \n{json}"
-        prompt = prompt.format(target_drug = self.drug_name,abstract=self.data["abstract"],json=json_object)
+        prompt = prompt.format(target_drug = self.data["base_info"]["drug_name"],abstract=self.data["abstract"],json=json_object)
         prompt_dict = {}
         prompt_dict["prompt"] = prompt
         result = gpt_request_from_relative_chunk(prompt_dict)
         try:
             json_object = json.loads(result)
+            self.data["answer"]["process_population_effectiveness"] = json_object
+            return json_object
         except json.JSONDecodeError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
-        return json_object
+        
 
     def ask_remain_question(self):
-        self.data["age"] = self.age()
-        self.data["disease"] = self.target_disease()
-        self.data["icd10"] = self.icd10_of_target_disease()
-        self.data["drug_intervention"] = self.drug_intervention()
-        self.data["study_type"] = self.study_type()
-        self.data["question_list"]["remain_problem"] = True
+        self.data["paper_info"]["age"] = self.age()
+        self.data["paper_info"]["disease"] = self.target_disease()
+        self.data["paper_info"]["icd10"] = self.icd10_of_target_disease()
+        self.data["paper_info"]["drug_route"] = self.drug_intervention()
+        self.data["paper_info"]["study_type"] = self.study_type()
+        self.data["state"]["remain_problem"] = True
 
     def age(self):
         prompt = ""
@@ -160,10 +244,12 @@ class Study:
         result = gpt_request_from_relative_chunk(prompt_dict)
         try:
             json_object = json.loads(result)
+            self.data["answer"]["age"] = json_object
+            return json_object
         except json.JSONDecodeError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
-        return json_object
+        
 
 
     def target_disease(self):
@@ -176,18 +262,21 @@ class Study:
         result = gpt_request_from_relative_chunk(prompt_dict)
         try:
             json_object = json.loads(result)
+            self.data["answer"]["target_disease"] = json_object
+            return json_object
         except json.JSONDecodeError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
-        return json_object
+
 
     def icd10_of_target_disease(self):
         if slef.data["disease"] == False:
-            self.data["disease"] = self.target_disease()
+            self.data["answer"]["disease"] = self.target_disease()
             disease =  self.data["disease"]
         else:
             disease =  self.data["disease"]
         icd10 = get_request_data_icd10(disease)
+        self.data["answer"]["icd10"] = icd10
         return icd10
 
     def drug_intervention(self):
@@ -200,10 +289,11 @@ class Study:
         result = gpt_request_from_relative_chunk(prompt_dict)
         try:
             json_object = json.loads(result)
+            self.data["answer"]["drug_intervention"] = json_object
+            return json_object
         except json.JSONDecodeError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
-        return json_object
 
     def study_type(self):
         prompt = ""
@@ -215,15 +305,15 @@ class Study:
         result = gpt_request_from_relative_chunk(prompt_dict)
         try:
             json_object = json.loads(result)
+            self.data["answer"]["study_type"] = json_object
+            return json_object
         except json.JSONDecodeError as e:
             logging.error(f"Error parsing JSON: {e}")
             return None
-        return json_object
+
 
     def related_chunk(self,question):
-        if self.data.content = False:
-            pass
-        else:
+        pass
 
         
 
--- main.py
+++ main.py
@@ -9,77 +9,92 @@ from utils.get_abstract_exist import get_abstract_exist
 from datetime import datetime
 from agents.study import Study
 import ipdb
-from llmlingua import PromptCompressor
+# from llmlingua import PromptCompressor
 # 示例研究数据
 current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 # 创建日志文件名
 log_filename = f"log_{current_time}.log"
-logger = CustomLogger("/LLM-Medical-Agent/log/"+log_filename)
-compressor = PromptCompressor(
-model_name="microsoft/llmlingua-2-xlm-roberta-large-meetingbank",
-use_llmlingua2=True
-)
+logger = CustomLogger("log/"+log_filename)
+# compressor = PromptCompressor(
+# model_name="microsoft/llmlingua-2-xlm-roberta-large-meetingbank",
+# use_llmlingua2=True
+# )
 
-with open("/LLM-Medical-Agent/data/pmid_with_name.jsonl","r",encoding="utf-8") as f:
-    """
-    pmid_with_name.jsonl:
-    {"英文通用名": ["Tobramycin", "dexamethasone"], "pmid": "12122630"}
-    ...
-    """
-    for line in f:
-        json_obj = json.loads(line.strip())
-        name = ""
-        for i in range(len(json_obj["英文通用名"])):
-            name =name + json_obj["英文通用名"][i]+"_"
-        name = name[:-1] 
-        pmid = json_obj["pmid"]
-        save_path = os.path.join("/LLM-Medical-Agent/data/answer_100(4)",name,pmid + ".json") 
-        if os.path.exists(save_path):
-            with open(save_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-                if data["question_list"]["process_pediatrics_incontent"] == False:
-                    continue
-        study_data = Study(pmid,name,compressor) 
+def contorl(name,pmid):
+    save_path = os.path.join("data/answer",name,pmid + ".json") 
+    if os.path.exists(save_path):
+        with open(save_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            if data["question_list"]["process_pediatrics_incontent"] == False:
+                return
+    study_data = Study(pmid,name) 
 
-        # 尝试获取摘要
-        abstract = get_abstract_exist(name,pmid)
-        if study_data.fetch_abstract(abstract):
-            # 流程控制
-            result = study_data.process_pediatrics_inabstract()
-            logger.log_info(f"{name}_{pmid} process_pediatrics_inabstract: {result}")
-            study_data.data["question_list"]["process_pediatrics_inabstract"] = True
-            if result["short_answer"].lower() == "included":
-                study_data.data["includes_pediatrics"] = True
-                result = study_data.process_effectiveness()
-                logger.log_info(f"{name}_{pmid} process_effectiveness: {result}")
-                study_data.data["question_list"]["process_effectiveness"] = True
-                if result["short_answer"].lower() == "yes":
-                    study_data.data["proves_effective"] = True
-                    # continue to ask question remain
+    # 尝试获取摘要
+    abstract = get_abstract_exist(name,pmid)
+    if study_data.fetch_abstract(abstract):
+        # 流程控制
+        result = study_data.process_pediatrics_inabstract()
+        logger.log_info(f"{name}_{pmid} process_pediatrics_inabstract: {result}")
+        study_data.data["state"]["process_pediatrics_inabstract"] = True
+        if result["short_answer"].lower() == "included":
+            study_data.data["paper_info"]["includes_pediatrics"] = True
+            result = study_data.process_effectiveness()
+            logger.log_info(f"{name}_{pmid} process_effectiveness: {result}")
+            study_data.data["state"]["process_effectiveness"] = True
+            if result["short_answer"].lower() == "yes":
+                study_data.data["paper_info"]["proves_effective"] = True
+                # continue to ask question remain
+                if study_data.data["state"]["include_content"] == True:
                     study_data.ask_remain_question()
-                    
-            elif result["short_answer"].lower() == "age_not_mentioned":
+                else:
+                    study_data.data["state"]["need_to_fetch_conent"] = True
+                
+        elif result["short_answer"].lower() == "age_not_mentioned":
+            if study_data.data["state"]["include_content"] == True:
                 result = study_data.process_pediatrics_incontent()   
                 logger.log_info(f"{name}_{pmid} process_pediatrics_incontent: {result}")
-                study_data.data["question_list"]["process_pediatrics_incontent"] = True
+                study_data.data["state"]["process_pediatrics_incontent"] = True
                 if result["short_answer"] == "yes": 
-                    study_data.data["includes_pediatrics"] = True
+                    study_data.data["paper_info"]["includes_pediatrics"] = True
                     result = study_data.process_population_effectiveness()
                     logger.log_info(f"{name}_{pmid} process_population_effectiveness: {result}")
-                    study_data.data["question_list"]["process_population_effectiveness"] = True
+                    study_data.data["state"]["process_population_effectiveness"] = True
                     if result["short_answer"] =="yes":
-                        study_data.data["proves_effective"] = True
+                        study_data.data["paper_info"]["proves_effective"] = True
                         # continue to ask question remain
                         study_data.ask_remain_question()
+            else:
+                study_data.data["state"]["need_to_fetch_conent"] = True
 
-            #save the answer to json
-            details = study_data.gather_details()
-            print(details)
-            if not os.path.exists(os.path.dirname(save_path)):
-                os.makedirs(os.path.dirname(save_path),exist_ok=True)
-            study_data.save_to_json(save_path)  # 保存到JSON文件
-        else:
-            if not os.path.exists(os.path.dirname(save_path)):
-                os.makedirs(os.path.dirname(save_path),exist_ok=True)
-            study_data.save_to_json(save_path)  # 保存到JSON文件
-            print("Failed to fetch abstract.")
+        #save the answer to json
+        details = study_data.gather_details()
+        print(details)
+        if not os.path.exists(os.path.dirname(save_path)):
+            os.makedirs(os.path.dirname(save_path),exist_ok=True)
+        study_data.save_to_json(save_path)  # 保存到JSON文件
+    else:
+        if not os.path.exists(os.path.dirname(save_path)):
+            os.makedirs(os.path.dirname(save_path),exist_ok=True)
+        study_data.save_to_json(save_path)  # 保存到JSON文件
+        print("Failed to fetch abstract.")
+
+if __name__ == "__main__":
+    with open("data/medicine_with_pmid.jsonl","r",encoding="utf-8") as f:
+        """
+        medicine_with_pmid.jsonl:
+        {"englishname": ["Tobramycin", "dexamethasone"], "data-chunk-ids: "12122630,23122451..."}
+        ...
+        """
+        for line in f:
+            json_obj = json.loads(line.strip())
+            name = ""
+            for i in range(len(json_obj["englishname"])):
+                name =name + json_obj["englishname"][i]+"-"
+            name = name[:-1] 
+
+            data_chunk_ids = json_obj["data-chunk-ids"]
+            for pmid in data_chunk_ids:
+                if pmid !="26059517":
+                    continue
+                contorl(name,pmid)
+            
